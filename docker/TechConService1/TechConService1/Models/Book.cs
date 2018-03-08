1c146f9bfd9bc6e7751f90a7271d13ac314e5b4d
Christian Nagel <christian@christiannagel.com>
2018-03-08 08:27:35 -0800

    tech conference 2018 sample

---
+++ docker/TechConService1/TechConService1/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechConService1.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+    }
+}

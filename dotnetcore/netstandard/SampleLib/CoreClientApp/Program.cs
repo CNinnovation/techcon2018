3e6cb01dd0d4f669568bf6901cf659b577b2b85f
Christian Nagel <christian@christiannagel.com>
2018-03-10 06:14:27 -0800

    .NET Standard lib

---
+++ dotnetcore/netstandard/SampleLib/CoreClientApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using SampleLib;
+using System;
+
+namespace CoreClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new Demo();
+            Console.WriteLine(demo.Greeting("Katharina"));
+            demo.ShowMessage("real?");
+        }
+    }
+}
---
+++ dotnetcore/netstandard/SampleLib/SampleLib/Demo.cs
@@ -0,0 +1,16 @@
+﻿using DotnetFrameworkLib;
+using System;
+
+namespace SampleLib
+{
+    public class Demo
+    {
+        public string Greeting(string name) => $"Hello, {name}";
+
+        public void ShowMessage(string message)
+        {
+            var dlg = new DialogService();
+            dlg.ShowMessage(message);
+        }
+    }
+}

f3556ca1552337478789e1db059020ea491c6663
Christian Nagel <christian@christiannagel.com>
2018-03-12 18:50:35 +0100

    ASP.NET Core 2.1

---
+++ dotnetcore/ASPNETCore21Data/ASDPNETCore21Data/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASDPNETCore21Data.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
